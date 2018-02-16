0120e631cc0104821f9afdb701d1c83ff2e7a1cc
plmercereau <24897252+plmercereau@users.noreply.github.com>
2018-02-16 19:08:19 +0000

    Redirect index to graphic view

--- platyplus/urls.py
+++ platyplus/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import RedirectView
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True)), name='graphql'),
+    path('', RedirectView.as_view(pattern_name='graphql')),
 ]
